1c4f45e579443e59d5238d201b24fbd56a265fc2
Andrew Zhu <me@andrew-zhu.com>
2019-01-20 01:08:37 -0800

    add some polishing, animations

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDLWu_MUM65r8j3lqUhIyj0YBNLFQYcgi4',
+    authDomain: 'project-anti-alz.firebaseapp.com',
+    databaseURL: 'https://project-anti-alz.firebaseio.com',
+    projectId: 'project-anti-alz',
+    storageBucket: 'project-anti-alz.appspot.com',
+    messagingSenderId: '381046946192'
+  },
+  apiUrl: 'https://project-anti-alz.appspot.com/'
 };
